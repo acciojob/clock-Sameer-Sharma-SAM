65e4dd3aca26f1b55d30a2cbc3d02ba5163bf78a
Sameer-Sharma-SAM <www.sameersharma12@gmail.com>
2025-10-29 10:15:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+    function updateTimer() {
+      const now = new Date();
+      const formatted = now.toLocaleString(); 
+      document.getElementById("timer").textContent = formatted;
+    }
+    updateTimer();
+    setInterval(updateTimer, 1000);
